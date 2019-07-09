9773b2f422bc580fd05b0431a05ac7dd6487924f
Meena <madhubalan.meena@gmail.com>
2019-07-09 17:14:16 +0530

    changed styles

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -4,7 +4,7 @@ function About() {
         <div className="card">
             <h1>About</h1>
             <h4><p>This is a Simple TODO Application. You can add and delete your todos !!! </p></h4>
-            <div><h1 className="bottom-right font-weight-bold"  >Thank You</h1></div>
+            <div><h1 className="bottom-right font-weight-bold font-italic"  >Thank You</h1></div>
         </div>
     )
 }
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react'
 function home() {
     return (
         <div className="container-fluid ">
-            <div><h1 className="centered font-weight-bold">Welcome</h1></div>
-            <div><h6 className="bottom-right "  >By Meena</h6></div>
+            <div><h1 className="centered font-weight-bold font-italic">Welcome</h1></div>
+            <div><h3 className="bottom-right "  >By Meena</h3></div>
             
         </div>
     )
